75e1fcb79aee28eed3c78f836a7d5bcd658295f2
geuno <rlfrmsdh1@gmail.com>
2022-12-01 11:50:26 +0900

    feat: Vue with js project init

---
+++ front/src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>로그인 페이지</p>
+</template>
+
+<script>
+export default {
+  name: 'LoginComponent'
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/components/TodoInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <v-text-field label="Add Todos" v-model="content" hide-details="auto" v-on:keyup.enter="addItem">
+      <v-icon slot="append" color="red" v-on:click="addItem">mdi-plus</v-icon>
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput'
+
+}
+</script>
+
+<style>
+
+</style>
